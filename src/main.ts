284ce3efdf464d7e4ec1c36b4f76d4108c398a71
Johannes Lingk <johannes.lingk@gmail.com>
2024-11-21 14:14:56 +0100

    Starting with renderer prototype

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.createElement("canvas");
+document.querySelector<HTMLDivElement>('#app')!.appendChild(canvas);
+const ctx = canvas.getContext("2d");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const parser = new DOMParser();
+const xmlDoc = parser.parseFromString("", 'text/xml');
+
+if (ctx) {
+  ctx.fillStyle = "rgb(255 0 0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
